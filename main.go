f597f5bdf8005f1b2513480438b1b99fa7f34cb0
Kevin Viricel <kevin.viricel@gmail.com>
2015-12-26 15:23:55 +0100

    Add whitelist mecanisms

--- main.go
+++ main.go
@@ -2,30 +2,16 @@ package main
 
 import (
 	"flag"
-	"io"
-	"log"
-	"os"
 	"strconv"
 )
 
 func main() {
 	port := flag.Int("p", 8000, "Proxy listening port")
-	reqLogfile := flag.String("rl", "", "Request logfile")
+	rLogPath := flag.String("rl", "", "Request logfile")
+	whitePath := flag.String("w", "", "Path to whitelist file")
 
 	flag.Parse()
 
-	var rLogWriter io.Writer
-	if *reqLogfile != "" {
-		rLogWriter, err := os.OpenFile(*reqLogfile,
-			os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer rLogWriter.Close()
-	} else {
-		rLogWriter = os.Stdout
-	}
-
-	p := newProxy(rLogWriter)
+	p := newProxy(*rLogPath, *whitePath)
 	p.run(strconv.Itoa(*port))
 }
--- proxy.go
+++ proxy.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"regexp"
 	"time"
 )
@@ -13,12 +14,58 @@ import (
 type proxy struct {
 	requestLogger  *log.Logger
 	routeWhiteList []*regexp.Regexp
-	routeBlackList []*regexp.Regexp
 }
 
-func newProxy(requestHandle io.Writer) *proxy {
-	l := log.New(requestHandle, "REQUEST: ", log.Ldate|log.Ltime)
-	return &proxy{requestLogger: l}
+// newProxy create a new instance of proxy.
+// It set request logger using rLogPath as output file or os.Stdout by default.
+// It also check whitelist file regexs validity
+func newProxy(rLogPath string, whitePath string) *proxy {
+	var l *log.Logger
+	var rwl []*regexp.Regexp
+
+	if rLogPath != "" {
+		f, err := os.OpenFile(rLogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer f.Close()
+		l = log.New(f, "REQUEST: ", log.Ldate|log.Ltime)
+	} else {
+		l = log.New(os.Stdout, "REQUEST: ", log.Ldate|log.Ltime)
+	}
+
+	if whitePath != "" {
+		fWhite, err := os.Open(whitePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer fWhite.Close()
+		s := bufio.NewScanner(fWhite)
+		for s.Scan() {
+			rgx, err := regexp.Compile(s.Text())
+			if err != nil {
+				log.Fatalln(err)
+			}
+			rwl = append(rwl, rgx)
+		}
+	}
+
+	return &proxy{requestLogger: l, routeWhiteList: rwl}
+}
+
+// checkWhiteList return true if r match with one regexp in whitelist file
+// or if any whitelist was set
+func (p *proxy) checkWhiteList(r string) bool {
+	if p.routeWhiteList == nil {
+		log.Println("NO WHITELIST DEFINED")
+		return true
+	}
+	for _, rgx := range p.routeWhiteList {
+		if rgx.MatchString(r) {
+			return true
+		}
+	}
+	return false
 }
 
 func (p *proxy) run(port string) {
@@ -28,6 +75,11 @@ func (p *proxy) run(port string) {
 			w.WriteHeader(http.StatusBadGateway)
 			return
 		}
+		if !p.checkWhiteList(host) {
+			p.requestLogger.Println(host, "FORBIDDEN")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
 
 		rp := httputil.NewSingleHostReverseProxy(&url.URL{
 			Scheme: "http",
@@ -35,10 +87,9 @@ func (p *proxy) run(port string) {
 		})
 		t := time.Now()
 		rp.ServeHTTP(w, r)
-		p.requestLogger.Println(r.URL.Path, host, time.Since(t))
+		p.requestLogger.Println(host, time.Since(t))
 	})
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
 	}
 }
